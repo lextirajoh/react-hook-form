129207f8aa5af2e28a614ba02494ee1b41649dc0
Lex <alext@riseup.net>
2023-02-06 00:30:14 +0100

    ...

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,42 +3,43 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
 export default function Form() {
-  const schema = yup.object().shape({
-    fullName: yup.string().required('Your Full Name is Required!'),
-    email: yup.string().email().required(),
-    age: yup.number().positive().integer().min(18).required(),
-    password: yup.string().min(4).max(20).required(),
-    confirmPassword: yup
-      .string()
-      .oneOf([yup.ref('password'), null], "Passwords Don't Match")
-      .required(),
-  });
+  const { register, handleSubmit } = useForm();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
+  function onSubmit() {}
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Full Name..." {...register('fullName')} />
-      <p>{errors.fullName?.message}</p>
-      <input type="text" placeholder="Email..." {...register('email')} />
-      <p>{errors.email?.message}</p>
-      <input type="number" placeholder="Age..." {...register('age')} />
-      <p>{errors.age?.message}</p>
-      <input type="password" placeholder="Password..." {...register('password')} />
-      <p>{errors.password?.message}</p>
-      <input type="password" placeholder="Confirm Password..." {...register('confirmPassword')} />
-      <p>{errors.confirmPassword?.message}</p>
-      <input type="submit" />
+      <label>
+        Full Name:
+        <br />
+        <input type="text" />
+      </label>
+
+      <label>
+        Email:
+        <br />
+        <input type="text" />
+      </label>
+
+      <label>
+        Age:
+        <br />
+        <input type="number" />
+      </label>
+
+      <label>
+        Password:
+        <br />
+        <input type="password" />
+      </label>
+
+      <label>
+        Confirm Password:
+        <br />
+        <input type="password" />
+      </label>
+
+      <button type="submit">Submit</button>
     </form>
   );
 }
