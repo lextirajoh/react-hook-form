5eb50835e36720bb2253578a3ca8d084d215a61f
Lex <alext@riseup.net>
2023-02-06 16:07:08 +0100

    Add basic functionality

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,45 +1,63 @@
 import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
 
 export default function Form() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    watch,
+  } = useForm<FormData>();
 
-  function onSubmit() {}
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
+  const username = watch('username');
+  const password = watch('password');
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>
-        Full Name:
-        <br />
-        <input type="text" />
-      </label>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Username:
+          <br />
+          <input type="text" {...register('username', { required: true })} />
+        </label>
+        {errors.username?.type === 'required' && (
+          <span className="error-message">Name is required.</span>
+        )}
 
-      <label>
-        Email:
-        <br />
-        <input type="text" />
-      </label>
+        <label>
+          Password:
+          <br />
+          <input type="password" {...register('password', { required: true, minLength: 4 })} />
+        </label>
+        {errors.password?.type === 'required' && (
+          <span className="error-message">Password is required.</span>
+        )}
+        {errors.password?.type === 'minLength' && (
+          <span className="error-message">Password must be more than 3 characters</span>
+        )}
 
-      <label>
-        Age:
-        <br />
-        <input type="number" />
-      </label>
+        <button type="submit">Register</button>
+        <button type="button" onClick={() => reset()}>
+          Reset
+        </button>
+      </form>
 
-      <label>
-        Password:
-        <br />
-        <input type="password" />
-      </label>
-
-      <label>
-        Confirm Password:
-        <br />
-        <input type="password" />
-      </label>
-
-      <button type="submit">Submit</button>
-    </form>
+      {(username || password) && (
+        <>
+          <h2>Preview:</h2>
+          <p>{username}</p>
+          <p>{password}</p>
+        </>
+      )}
+    </>
   );
 }
+
+type FormData = {
+  username: string;
+  password: string;
+};
